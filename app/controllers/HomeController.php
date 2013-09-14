97b81d396c834f00fda0f0f085312451038b4a6f
Christopher Paslawski <chris@paslawski.me>
2013-09-14 02:32:18 -0400

    Update to Laravel 4.0.7

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,10 +2,23 @@
 
 class HomeController extends BaseController {
 
-    /**
-     * The layout that should be used for responses.
-     */
-    protected $layout = 'layouts.master';
+	/*
+	|--------------------------------------------------------------------------
+	| Default Home Controller
+	|--------------------------------------------------------------------------
+	|
+	| You may wish to use controllers instead of, or in addition to, Closure
+	| based routes. That's great! Here is an example controller method to
+	| get you started. To route to this controller, just add the route:
+	|
+	|	Route::get('/', 'HomeController@showWelcome');
+	|
+	*/
+
+	/**
+	* The layout that should be used for responses.
+	*/
+	protected $layout = 'layouts.master';
 
 	public function getIndex()
 	{
